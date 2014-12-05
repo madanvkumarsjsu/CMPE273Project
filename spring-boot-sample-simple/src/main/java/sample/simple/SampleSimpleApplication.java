c2115f0dc52c68b703047e6677dee38b196a7346
madanvkumarsjsu <madankumar.vijayakumar@sjsu.edu>
2014-12-05 14:24:52 -0800

    Changes to simple sample application
    Included Etcddition

--- spring-boot-sample-simple/src/main/java/sample/simple/SampleSimpleApplication.java
+++ spring-boot-sample-simple/src/main/java/sample/simple/SampleSimpleApplication.java
@@ -2,57 +2,46 @@ package sample.simple;
 
 import static org.boon.Boon.puts;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
-import java.util.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import org.boon.etcd.ClientBuilder;
 import org.boon.etcd.Etcd;
 import org.boon.etcd.Response;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.hibernate.validator.internal.util.privilegedactions.GetClassLoader;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.sjsu.etcd.HeartBeat;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class SampleSimpleApplication {
 
+	
 	public static void main(String[] args) {
-System.out.println(">>>>>>>>>>Inside the main method");
-        ServerUtility su = new ServerUtility();
-        final String hostAddr = su.getHostAddress();
-        final String hostName = su.getHostName();
-        final String portNumber = su.getPortNumber();
-
-        ApplicationContext ctx = SpringApplication.run(SampleSimpleApplication.class, args);
-        final Etcd client = ClientBuilder.builder().hosts(URI.create("http://localhost:4001")).createClient();
-
-        System.out.println(">>>>>>>>>>>>>starting the ttl");
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-                System.out.println(new Date());
-                JSONObject jo = new JSONObject();
-                String value = null;
-                try {
-                    jo.put("ip address", hostAddr);
-                    jo.put("port", portNumber);
-                    jo.put("host name", hostName);
-                    value = jo.toString();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-                Response response2 = client.setTemp("spring-boot-simple-sample1/" + portNumber, value, 10);
-                puts(response2);
-            }
-		}, 0, 15000);
+		ApplicationContext ctx = SpringApplication.run(SampleSimpleApplication.class, args);
+		//File f = new File("src/main/resource/application.properties");
+		
+		Etcd client = ClientBuilder.builder().hosts(URI.create("http://localhost:4001")).createClient();
+		Response response = client.setTemp("application","mysampleapplication1",30);
+		HeartBeat hb = new HeartBeat("database");
+		hb.start();
+		
 	}
 
 }
