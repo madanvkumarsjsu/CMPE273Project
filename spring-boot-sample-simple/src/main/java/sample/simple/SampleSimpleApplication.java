55b9e99f85fa9a16889175410b6dea9b8905cb91
madanvkumarsjsu <madankumar.vijayakumar@sjsu.edu>
2014-12-07 18:19:17 -0800

    Final changes
    Final Changes

--- spring-boot-sample-simple/src/main/java/sample/simple/SampleSimpleApplication.java
+++ spring-boot-sample-simple/src/main/java/sample/simple/SampleSimpleApplication.java
@@ -1,38 +1,13 @@
 package sample.simple;
 
-import static org.boon.Boon.puts;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.boon.etcd.ClientBuilder;
-import org.boon.etcd.Etcd;
-import org.boon.etcd.Response;
-import org.hibernate.validator.internal.util.privilegedactions.GetClassLoader;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import com.sjsu.etcd.EtcdInitializer;
-import com.sjsu.etcd.HeartBeat;
 import com.sjsu.etcd.ServiceXMLParser;
 
 @Configuration
@@ -48,7 +23,7 @@ public class SampleSimpleApplication {
 		try {
 			System.out.println(res.getFile());
 			EtcdInitializer client = new EtcdInitializer("localhost");
-			ServiceXMLParser sxp = new ServiceXMLParser(client.getClient(),res.getFile());// for testing
+			ServiceXMLParser sxp = new ServiceXMLParser(client.getClient(),res.getFile());
 			sxp.parseServiceXML();
 			client.getService(client.getClient(), sxp.getStrApplicationName(), "database");
 		} catch (Exception e) {
