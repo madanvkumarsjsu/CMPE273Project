981f3228a2b29d4cac22a0ae2277d188233a368f
Lavanya <akshaynm@akshaynm-laptop.(none)>
2014-12-07 22:44:37 -0800

    modified loadbalancer

--- LoadBalancer/loadBalancer.java
+++ LoadBalancer/loadBalancer.java
@@ -1,4 +1,4 @@
-package etcdClient;
+package loadBalancer;
 
 
 import org.boon.core.Sys;
@@ -7,18 +7,23 @@ import org.boon.etcd.Etcd;
 import org.boon.etcd.Response;
 import org.boon.etcd.Node;
 
-import java.net.URI;
+import java.net.*;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 
 import static org.boon.Boon.puts;
 import static org.boon.Exceptions.die;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+import java.util.concurrent.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import org.json.JSONObject;
 import java.util.*;
@@ -27,40 +32,67 @@ import java.io.*;
 
 @Controller
 @RequestMapping("/v1/api")
-public class loadBalancer {
+public class loadBalancer{
 
-    Etcd client;
-    loadBalancer(){
+    Etcd client; 
+    ConcurrentLinkedQueue<String> serverQueue;
+
+    loadBalancer() {
         Response response;
-        JSONObject jo = new JSONObject();
-        try{jo.put("ip addr", "127.0.0.1");
-        jo.put("port", "8080");
-        }catch(Exception e){
-
-        }
-        String value = jo.toString();
-        System.out.println(value);
-        etcdSmartClient client = new etcdSmartClient("loadbalancer",value,"loadbalancer");
-        client.Start();
-        //client.getAllKeys();
-
+        client = ClientBuilder.builder().hosts(
+                URI.create("http://localhost:4001")).createClient();
+        serverQueue = new ConcurrentLinkedQueue<String>();
     }
+
 
     @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody String getReq(){
-      
+    public @ResponseBody ResponseEntity<String> onRequest(){
+      String nextServer = null;
+      ResponseEntity<String> result=null;
       Response response = client.listRecursive("Services");
-      Node firstNode = response.node();
-      List<Node> nodes = response.node().getNodes();
-      nodes.add(firstNode); 
-      List<String> keyJson = new ArrayList<String>();
-      for(ListIterator<Node> iter = nodes.listIterator(); iter.hasNext(); ) {
-        Node node = iter.next();
-        keyJson.add(node.getValue());
-        System.out.println(node.getValue());
+      List<Node> activeNodes = response.node().getNodes();
+      //List<String> keyJson = new ArrayList<String>();
+      if(activeNodes.isEmpty()){
+        System.out.println("No services available");
+        return(result);
       }
-      return("done");
-    }
+      for(ListIterator<Node> iter = activeNodes.listIterator(); iter.hasNext(); ) {
+        Node node = iter.next();
+        if(!serverQueue.contains(node.key()))
+        {
+          System.out.println("adding node which is not present in serverQ");
+          serverQueue.add(node.key());   
+        }
+        //keyJson.add(node.getValue());
+        System.out.println(node.getValue());
+      } 
+      while(!(serverQueue.isEmpty()))
+      {
+        nextServer = serverQueue.poll();
+        System.out.println(nextServer);
+        if((client.get(nextServer)).node()==null){
+          //serverQueue.remove(nextServer);
+          System.out.println("Removing service from queue: it is not active");
+        }
+        else{
+          System.out.println("Got the active server:");
+          break;
+        }
+      }
+      
+       
+      String serverAddr = (client.get(nextServer.substring(1))).node().getValue();
+      JSONObject server = new JSONObject(serverAddr);
+      System.out.println(server);
 
+      String url_string = "http://"+server.getString("hostname")+":"+server.getString("port") + "/count";
+      System.out.println(url_string);
+
+   RestTemplate restTemplate = new RestTemplate();
+   result = restTemplate.getForEntity(url_string,String.class);
+
+   return(result);
+
+  }
 }
 
